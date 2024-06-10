d906f383027891873b3f1bd0ea849bed4a0b10c0
gabrielboeskov <gabriel.boeskov@gmail.com>
2024-06-10 21:12:38 +0200

    .

--- Main.r
+++ Main.r
@@ -1,6 +1,3 @@
-# Filepath <- 
-# setwd(Filepath)
-
 rm(list = ls())
 
 library(ggplot2)
@@ -18,6 +15,7 @@ set.seed(123)
 # Data load ind
 skip = 5
 
+#data files
 df <- read.csv("data/Syn_Greenland.csv",sep=",",skip = skip)
 df_in <- read.csv("data/China_cave.csv",sep=",",skip = skip)
 
@@ -35,7 +33,7 @@ preprocess_data <- function(data) {
     mutate(d18O = d18O - rolling_mean) %>%
     
     select(-rolling_mean) %>%
-
+    
     arrange(desc(age))
   
   return(data.frame(return_df))
@@ -61,7 +59,8 @@ F_drift <- function(X, beta1,beta2,beta4){
   return(beta4*X^3 - beta2*X - beta1)
 }
 
-plot(seq(-10,10,0.01),F_drift(seq(-10,10,0.01), 0.03,  0.08, -0.06), type='l')
+# visualize drift function
+plot(seq(-2,2,0.01),F_drift(seq(-2,2,0.01), 1,  1, 0.5), type='l')
 
 F_jump <- function(x, x_val, alpha) {
   jump <- 1 + exp((x_val-x)/alpha)
@@ -81,7 +80,7 @@ T_start <- df_pro[length(df_pro[,1]),1]
 T_end <- df_pro[1,1]
 x0=0
 
-# Euler-Maruyama negative log likelihood
+# Euler-Maruyama negative log likelihood for jump-diffusion model
 EM_nll_jump <- function(theta, X, dt) {
   N <- length(X) - 1
   beta1<-theta[1]
@@ -114,32 +113,30 @@ EM_nll_jump <- function(theta, X, dt) {
   return(nll)
 }
 
-#                            β1,             β2,         β3,          σ,      λ    γ,   x∗
-initial_beta_jump_bfsg <- c(-0.00447839, 0.03581118, -0.00069272, 0.55070983, 0.1, 0.1, 0.1)
+# optimizing algorithm for finding MLE parameters
+#                            beta1,        beta2,       beta3,      sigma, lambda,gamma, x∗
+initial_beta_jump_bfsg <- c(−0.00447839, 0.03581118, −0.00069272, 0.55070983, 1, 2, 1.8)
 
 result_EM_1_jump_bfsg <- optim(par = initial_beta_jump_bfsg, 
-                          fn = EM_nll_jump, 
-                          X = df_pro[,2], 
-                          dt = 0.05,
-                          control=list(maxit=1000),
-                          method='L-BFGS-B',
-                          lower=c(-5,-5,-5,0.0001,0.0001,-5,-5),
-                          upper=c(5,5,5,5,5,5,5))
+                               fn = EM_nll_jump, 
+                               X = df_pro[,2], 
+                               dt = 0.05,
+                               control=list(maxit=1000),
+                               method='L-BFGS-B',
+                               lower=c(-5,-5,-5,0.0001,0.0001,-5,-5),
+                               upper=c(5,5,5,5,5,5,5))
 
 
-
-?optim
 initial_beta_jump <- result_EM_1_jump_bfsg$par
 (result_EM_1_jump <- optim(par = initial_beta_jump_bfsg, 
-                          fn = EM_nll_jump, 
-                          X = df_pro[,2], 
-                          dt = 0.05,
-                          control=list(maxit=1000)))
+                           fn = EM_nll_jump, 
+                           X = df_pro[,2], 
+                           dt = 0.05,
+                           control=list(maxit=1000)))
 
 result_jump <- result_EM_1_jump$par
-result_jump <- c(-0.01073449,  0.04937238,  0.02828193, 0.63363157, 
-                 0.04917792,  0.18292174,  0.03550532)
 
+# defining T, dt X_0 and iterations for bootstrap analysis
 T <- length(df_pro[,2])*0.05
 dt <- 0.05
 x0 <- 0
@@ -163,46 +160,17 @@ simulate_EM_jump <- function(T, dt, b1, b2, b4, sigma, lambda, alpha, x_val, x0)
   return(data.frame(t = T - t, X = X))  # Adjust time vector for correct plotting
 }
 
-simulate_Milstein <- function(T, dt, b1, b2, b4, sigma, lambda, alpha, x_val, x0) {
-  t <- seq(0, T, by = dt)
-  n <- length(t) - 1
-  dW <- rnorm(n) * sqrt(dt)  # Correct Wiener increment
-  dN <- rpois(n, lambda * dt)  # Poisson increment
-  X <- numeric(n + 1)
-  X[1] <- x0
-  
-  # Define the diffusion function
-  diffusion_term <- sigma
-  
-
-  for (i in 2:(n + 1)) {
-    drift_term <- F_drift(X[i-1], b1, b2, b4)
-    jump_term <- F_jump(X[i-1],x_val, alpha)
-    
-    # Milstein scheme with jump term
-    X[i] <- X[i-1] + drift_term * dt + diffusion_term * dW[i-1] +
-      0.5 * diffusion_term * (dW[i-1]^2 - dt) + dN[i-1] * jump_term
-  }
-  
-  return(data.frame(t = T - t, X = X))  # Adjust time vector for correct plotting
-}
-
+# plot of simulated data with MLE parameters and subsampling
 a <- simulate_EM_jump(T, dt/10, result_jump[1], result_jump[2],result_jump[3], 
-                 result_jump[4], result_jump[5], result_jump[6], result_jump[7],x0)
+                      result_jump[4], result_jump[5], result_jump[6], result_jump[7],x0)
 
 plot(a[seq(1, nrow(a), 10), ]$t, a[seq(1, nrow(a), 10), ]$X,type='l', col='red',
      xlab = "Time before present (kyrs)",ylab = expression(deltaˆ18 ~ O ~ (permil)))
 
-
-b <- simulate_Milstein(T, dt, result_jump[1], result_jump[2],result_jump[3], 
-                  result_jump[4], result_jump[5], result_jump[6], result_jump[7]
-                  ,x0)
-plot(b$t, b$X,type='l', col='red')
-
-
+#plot of observed data 
 plot(df_pro[,1], df_pro[,2], type='l')
 
-
+# Euler-Maruyama negative log likelihood for diffusion model
 EM_nll <- function(theta, X, dt) {
   N <- length(X) - 1
   beta1<-theta[1]
@@ -228,11 +196,14 @@ EM_nll <- function(theta, X, dt) {
   return(nll)
 }
 
+# optimizing for MLE parameters in diffusion model
 initial_beta <- c(1,1,1,1)
 (result_EM_1 <- optim(par = initial_beta, fn = EM_nll,
-                     X = df_pro[,2], dt = 0.05))
+                      X = df_pro[,2], dt = 0.05))
 result <- result_EM_1$par
 
+
+# simulating data with the MLE parameters
 simulate_EM <- function(T, dt, b1, b2, b4, sigma, x0) {
   t <- seq(0, T, by = dt)
   n<-length(t)-1
@@ -245,18 +216,21 @@ simulate_EM <- function(T, dt, b1, b2, b4, sigma, x0) {
   }
   return(data.frame(t = T-t, X = X))
 }
-
+# Subsampling
 step <- simulate_EM(800, dt/10, result[1], result[2], 
-                              result[3],result[4], x0)
+                    result[3],result[4], x0)
 
+# taking every 10'th value since we produces 10 times as many data points per subsampling
 result_eul_11d <- step[seq(1, nrow(step), 10), ]
 
+# plot of simulated data versus observed data
 plot(df_pro[,1],df_pro[,2], type='l', col='black')
 lines(result_eul_11d, type='l', col='darkblue')
 
-plot(result_eul_11d, type='l', col='darkblue',
-     xlab = "Time before present (kyrs)",ylab = expression(deltaˆ18 ~ O ~ (permil)))
+# plot of simulated data formatted nicely
+plot(result_eul_11d, type='l', col='darkblue', xlab = "Time before present (kyrs)",ylab = expression(deltaˆ18 ~ O ~ (permil)))
 
+# Density plots of both models versus observed data
 plot(density(df_pro[,2]), col='darkred', main="", xlab=expression(deltaˆ18 ~ O ~ (permil)))
 lines(density(a[seq(1, nrow(a), 10), ]$X), main="")
 
@@ -264,11 +238,10 @@ plot(density(result_eul_11d[1:length(df_pro[,2]),2]),main="", xlab=expression(de
 lines(density(df_pro[,2]), col='darkred')
 
 
-
+# qqplot of simulated data versus observed data for both models
 qqplot(a[seq(1, nrow(a), 10), ]$X,df_pro[,2], xlab = "Simulated data", ylab = "observed data")
 
 abline(lm(sort(df_pro[,2]) ~ sort(a[seq(0, nrow(a), 10), ]$X)), col = "darkred")
-
 
 qqplot(result_eul_11d[1:length(df_pro[,2]),2],df_pro[,2], 
        xlab = "Simulated data", ylab = "observed data")
@@ -276,17 +249,17 @@ qqplot(result_eul_11d[1:length(df_pro[,2]),2],df_pro[,2],
 abline(lm(sort(df_pro[,2]) ~ sort(result_eul_11d[1:length(df_pro[,2]),2])), col = "darkred")
 
 
-
+# Bootstrap analysis of the MLE parameters 
 boot_params <- function(iter, init_params, mult){
   boot_res <- matrix(0, iter, length(init_params)+1)
   boot_res[1,] <- c(init_params,0)
   
   for (i in 2:iter){
     data <- simulate_EM(800, dt/mult, boot_res[1,1], boot_res[1,2], 
-                         boot_res[1,3],boot_res[1,4], x0)
-
+                        boot_res[1,3],boot_res[1,4], x0)
+    
     optim_data <- data[seq(1, nrow(data), 10), ]
-
+    
     res <- optim(par = boot_res[1,1:4], fn = EM_nll,
                  X = optim_data$X, dt = 0.05)
     
@@ -303,22 +276,45 @@ boot_params_jump <- function(iter, init_params, mult){
   
   for (i in 2:iter){
     data <- simulate_EM_jump(800, dt/mult, boot_res[1,1], boot_res[1,2], 
-                        boot_res[1,3],boot_res[1,4],
-                        boot_res[1,5],boot_res[1,6],
-                        boot_res[1,7], x0)
+                             boot_res[1,3],boot_res[1,4],
+                             boot_res[1,5],boot_res[1,6],
+                             boot_res[1,7], x0)
     
     optim_data <- data[seq(1, nrow(data), 10), ]
     
     res <- optim(par = boot_res[1,1:7], fn = EM_nll_jump,
                  X = optim_data$X, dt = 0.05)
-
+    
     boot_res[i,] <- c(res$par, res$convergence)
   }
   return(boot_res)
 }
+Boot_jump <- boot_params_jump(100, result_jump, 10)
+Boot_jump
 
+quantile(Boot_jump[,2], c(0.025, 0.975))
+quant_jump <- matrix(0,2,7)
+for (i in 1:7){
+  quant_jump[,i] <- quantile(Boot_jump[,i], c(0.025, 0.975))
+}
 
+quantile(Boot[,2], c(0.025, 0.975))
+quant <- matrix(0,2,4)
+for (i in 1:4){
+  quant[,i] <- quantile(Boot[,i], c(0.025, 0.975))
+}
 
+par(mfrow=c(2,4))
+
+plot(density(Boot_jump[,1]), main='beta1')
+plot(density(Boot_jump[,2]), main='beta2')
+plot(density(Boot_jump[,3]), main='beta3')
+plot(density(Boot_jump[,4]), main='sigma')
+plot(density(Boot_jump[,5]), main='lambda')
+plot(density(Boot_jump[,6]), main='gamma')
+plot(density(Boot_jump[,7]), main='x*')
+
+# transition Densities calculated for each datapoint but not visualized in qq plot since i could not figure out how to # compare to convolution of Poisson- and normal-distribution
 trans_dens_jump <- function(X,dt,theta){
   n <- length(X)-1
   beta1<-theta[1]
@@ -329,88 +325,32 @@ trans_dens_jump <- function(X,dt,theta){
   alpha <- theta[6]
   x_val <- theta[7]
   
-  drifts <- F_drift(X, beta1,beta2,beta4)
-  dif <- sigma^2*dt
-  jumps <- F_jump(X, x_val, alpha)
   
-  trans <- numeric(n)
-  for (i in 2:(n+1)){
-    jumps_track <- 0
-    for (j in 1:10){
-      jumps_track <- jumps_track + ((1/(sqrt(2*pi*dif)))*
-                                      exp(-(1/2)*(X[i]-X[i-1]-drifts[i-1]*dt-jumps[i-1]*j)^2/(dif)) * 
-                                      (((lambda * dt)^j/factorial(j))*exp(-lambda*dt)))
-    }
-    trans[i-1] <- jumps_track
+  jump <- F_jump(X, x_val, alpha)
+  drift <- F_drift(X, beta1,beta2,beta4)
+  
+  mean_val <- X + unlist(lapply(drift, function(df) df * dt))
+  
+  jump_mean <- unlist(lapply(jump, function(df) df*c(0:10)))
+  
+  var_val <- sigma^2*dt
+  
+  density_vals <- matrix(0,nrow = length(X), ncol = 11)
+  for (i in 0:10){
+    density_vals[,i+1] <- pnorm(q = X, mean = mean_val + jump_mean[seq(1+i, length(jump_mean), 11)], 
+                                sd = sqrt(var_val)) * ppois(q = i, lambda * dt)
+    
   }
-  return(trans)
+  return(rowSums(density_vals))
 }
 
-result_jump
+# calculating transition densities
+dens <- trans_dens_jump(df_pro[,2], dt, result_jump)
 
-plot(seq(-20,20,0.01), F_drift(seq(-20,20,0.01), result_jump[1],result_jump[2],result_jump[3]))
-
-a <- simulate_EM_jump(T, dt/10, 0.003, 0.05,-0.005, 
-                      0.1, 1, 2, 1,x0)
-
-plot(a[seq(1, nrow(a), 10), ]$t, a[seq(1, nrow(a), 10), ]$X,type='l', col='red',
-     xlab = "Time before present (kyrs)",ylab = expression(deltaˆ18 ~ O ~ (permil)))
+# Plotting density of transition densities
+plot(density(dens))
 
 
-trans_dens <- trans_dens_jump(df_pro[,2], dt, result_jump)
-plot(density(trans_dens))
-
-Boot_jump <- boot_params_jump(100, result_jump, 10)
-Boot_jump
-
-quantile(Boot_jump[,2], c(0.025, 0.975))
-quant_jump <- matrix(0,2,7)
-for (i in 1:7){
-  quant_jump[,i] <- quantile(Boot_jump[,i], c(0.025, 0.975))
-}
-
-quant_jump
-
-plot(density(Boot_jump[,7]))
-
-quantile(Boot[,2], c(0.025, 0.975))
-quant <- matrix(0,2,4)
-for (i in 1:4){
-  quant[,i] <- quantile(Boot[,i], c(0.025, 0.975))
-}
-
-quant_jump
-names(data.frame(quant))
-
-par(mfrow=c(2,4))
-
-
-plot(density(Boot_jump[,1]), main='beta1')
-plot(density(Boot_jump[,2]), main='beta2')
-plot(density(Boot_jump[,3]), main='beta3')
-plot(density(Boot_jump[,4]), main='sigma')
-plot(density(Boot_jump[,5]), main='lambda')
-plot(density(Boot_jump[,6]), main='gamma')
-plot(density(Boot_jump[,7]), main='x*')
-
-
-#Plot af boot data
-
-ggplot(data = data.frame(quant), aes(x = forcats::fct_inorder(Variable))) +
-  geom_point( aes(x = forcats::fct_inorder(Variable), y = Optimal), shape = 16, color = "green") +
-  geom_errorbar(aes(ymin=Lower, ymax=Upper), colour="black", width=.1, ) +
-  geom_point( aes(x = forcats::fct_inorder(Variable), y = Mean), shape = 16, color = "black") +
-  labs(x = "Variable", y = "Estimated value") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) +
-  theme(panel.margin = unit(1, "lines")) +  # Adjust panel margin to prevent clipping
-  theme(axis.ticks.x = element_blank())  # Remove x-axis ticks
-
-
-
-a <- c()
-for (i in 1:1000){
-  a[i] <- sum(rpois(15237,60*dt))
-}
-
-1-sum(dpois(0:10,60*0.05))
-plot(density(a))
+# calculating the accuracy lost by truncating the likelihood of more then 10 jumps in the MLE for the jump diffusion model with lambda = 59.99457
+Pois_trunc <- 1-sum(dpois(0:10,59.99457*0.05))
+print(Pois_trunc)
